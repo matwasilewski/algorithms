d9d1ebf16f5226716eea8a2a930dfb36c597afc5
santiagonasar <mat.p.wasilewski@gmail.com>
2019-12-04 02:55:48 +0000

    minor cosmetic improvements, plus method for testing InsertSort added

--- python/AlgorithmTester.py
+++ python/AlgorithmTester.py
@@ -1,6 +1,7 @@
 import numpy as np
 from math import ceil
 from QuickSortHoarePartitionScheme import QuickSort as QuickSortHoare
+from InsertionSort import InsertionSort
 import matplotlib.pyplot as plt
 
 
@@ -12,6 +13,22 @@ class AlgorithmTester:
     def generate(self):
         arr = list(np.random.randint(0, ceil(self.size / 2), self.size))
         return arr
+
+    def verify_inserion_sort(self):
+        ok = True
+
+        for i in range(self.iterations):
+            arr = self.generate()
+            isort = InsertionSort(arr)
+            result = isort.sortAndVerify()
+
+            if not result:
+                isort.print_report()
+                ok = False
+        if ok:
+            print("Insertion Sort: sorting succesfull!")
+        else:
+            print("Insertion Sort:  There were errors")
 
     def verify_qs_hoare(self):
         qs_calls_values = np.arange(self.iterations)
@@ -27,10 +44,10 @@ class AlgorithmTester:
                 qs.print_report()
                 ok = False
         if ok:
-            print("Sorting succesfull!")
-            self.generate_calls_plot(qs_calls_values)
+            print("QuickSort (hoare): Sorting succesfull!")
+            # self.generate_calls_plot(qs_calls_values)
         else:
-            print("There were errors")
+            print("QuickSort (hoare): There were errors")
 
     def generate_calls_plot(self, array):
         plt.hist(array)
@@ -40,3 +57,4 @@ class AlgorithmTester:
 
 tester = AlgorithmTester(30, 50)
 tester.verify_qs_hoare()
+tester.verify_inserion_sort()
