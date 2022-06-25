3fc150a2a07d71eb332a4ff67334701fca09ad69
mateusz.wasilewski2 <mateusz.wasilewski@biorelate.com>
2022-06-25 23:43:54 +0100

    AbstractSort adjustments to logging

--- python/src/sort/AbstractSort.py
+++ python/src/sort/AbstractSort.py
@@ -5,14 +5,19 @@ from utils.logging import log
 
 
 class AbstractSort(ABC):
-    def sort(self, iterable: Iterable) -> Iterable:
+    def sort_and_time(self, iterable: Iterable) -> [Iterable, float]:
         ts = time.time()
         result = self._sort(iterable)
         te = time.time()
+        t = te - ts
 
-        log.info(
-            f"{self.__class__.__name__} (N: {len(iterable)}) {te - ts} sec"
-        )
+        log.info(f"{self.__class__.__name__} (N: {len(iterable)}) {t} sec")
+
+        return result, t
+
+    def sort(self, iterable: Iterable) -> Iterable:
+        result, _ = self.sort_and_time(iterable)
+
         return result
 
     @abstractmethod
