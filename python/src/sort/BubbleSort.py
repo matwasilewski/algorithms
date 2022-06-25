496f488dcbe4d63eb9f432e1819d80986f1fe8dc
mateusz.wasilewski2 <mateusz.wasilewski@biorelate.com>
2022-06-25 13:12:33 +0100

    add bubblesort

--- python/src/sort/BubbleSort.py
+++ python/src/sort/BubbleSort.py
@@ -1,31 +1,18 @@
 import numpy
 from copy import copy
 
+from sort.AbstractSort import AbstractSort
 
-class BubbleSort:
-    def __init__(self, arr):
-        self.arr = arr
-        self.arr_copy = copy(self.arr)
-        self.arr_sorted = sorted(copy(self.arr))
-        self.counter = 0
 
-    def sortAndVerify(self):
-        self.bubble_sort(self.arr)
-        return self.verify(self.arr)
-
-    def verify(self, arr):
-        return arr == self.arr_sorted
-
-    def print_report(self):
-        print(self.arr_copy)
-        print(self.arr)
-        print(self.arr_sorted)
-
-    def swap(self, arr, i, j):
+class BubbleSort(AbstractSort):
+    @staticmethod
+    def swap(arr, i, j):
         arr[i], arr[j] = arr[j], arr[i]
 
-    def bubble_sort(self, arr):
+    @staticmethod
+    def sort(arr):
         for i in range(len(arr) - 1):
             for j in range(len(arr) - 1):
                 if arr[j] > arr[j + 1]:
-                    self.swap(arr, j, j + 1)
+                    BubbleSort.swap(arr, j, j + 1)
+        return arr
